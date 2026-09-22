f985ac62ab99f9fb2af36ab38d97ee5f5003b85e
Skye Kaden <kaden.s@northeastern.edu>
2026-09-21 21:42:14 -0400

    print name and email 20 times

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Skye\n");
-  printf("kaden.s@northeastern.edu\n");
+  for (int i = 0; i < 20; i++) {
+    printf("Skye\n");
+    printf("kaden.s@northeastern.edu\n");
+  }
 
   return 0;
 }
